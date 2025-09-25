a80317e1ace0ec5f760f9741255fa2e0628036ee
bAnahi <vbochec1@miumg.edu.gt>
2025-09-24 22:39:09 -0600

    Initial commit

---
+++ prograiII25Grupo7B/src/main/java/com/prograii25/prograiii25grupo7b/PrograiII25Grupo7B.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.prograii25.prograiii25grupo7b;
+
+/**
+ *
+ * @author Famil
+ */
+public class PrograiII25Grupo7B {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
